163e15c5ba888d9f406df7a4fc2f34216a42bbde
adriquint <125844291+adriquint@users.noreply.github.com>
2023-05-19 22:22:53 -0300

    Add files via upload

--- ejercicios_profundizacion/js/profundizacion_1.js
+++ ejercicios_profundizacion/js/profundizacion_1.js
@@ -12,3 +12,13 @@
  *    Debe recibir 5 números como argumento y calcular el promedio.  Debe reportar por
  *    medio de alert(); y console.log(); el detalle de los datos recibidos.
 */
+function promedio(numero1,numero2,numero3, numero4, numero5) {
+   alert(`Se ingresaron los números: ${numero1}, ${numero2}, ${numero3}, ${numero4} y ${numero5}`);
+   console.log(`Se ingresaron los números: ${numero1}, ${numero2}, ${numero3}, ${numero4} y ${numero5}`);
+   alert(`El promedio de números ingresados es: ${(numero1+numero2+numero3+numero4+numero5)/5}`);
+   console.log(`El promedio de números ingresados es: ${(numero1+numero2+numero3+numero4+numero5)/5}`);
+}
+
+promedio (8,4,5,2,3)
+promedio (12,-9,0,-23,7)
+
--- ejercicios_profundizacion/js/profundizacion_2.js
+++ ejercicios_profundizacion/js/profundizacion_2.js
@@ -13,3 +13,31 @@
  *    Según la cantidad especificada utilice condicionales para solicitar la cantidad de nombres
  *    correctos.
 */
+
+let cantidad = 0;
+let invitado1 = "";
+let invitado2 = "";
+let invitado3 = "";
+
+
+function generarInvitados(){
+    if(cantidad = 1) {
+        invitado1 = prompt("Ingrese el nombre de su invitado");
+        alert(`Su invitado es ${invitado1}`)
+        return invitado1;       
+    } else if (cantidad=2) {
+        invitado1 = prompt("Ingrese el nombre de un invitado");
+        invitado2 = prompt("Ingrese el nombre del segundo invitado");
+        return `${invitado1} y ${invitado2}`;
+    } else if (cantidad=3) {
+        let invitado1 = prompt("Ingrese el nombre de un invitado");
+        let invitado2 = prompt("Ingrese el nombre del segundo invitado");
+        let invitado3 = prompt("Ingrese el nombre del tercer invitado");
+        return `${invitado1}, ${invitado2} y ${invitado3}`;
+    }        
+}
+
+parseInt(prompt(`Ingrese la cantidad de invitados (1, 2 o 3):`));
+
+let invitados = generarInvitados();
+alert(`Sus invitados son: ${invitados}`)
